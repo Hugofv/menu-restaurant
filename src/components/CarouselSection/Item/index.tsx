52de72e2b79a26735fae5b731bc9beee1603256d
Hugo Fernandes <hugo.fernandes@magnumbank.com.br>
2024-06-24 09:02:27 -0300

    Improve check element is visible.

--- src/components/CarouselSection/Item/index.tsx
+++ src/components/CarouselSection/Item/index.tsx
@@ -11,23 +11,22 @@ interface IItemProps {
 
 const Item: React.FC<IItemProps> = ({ active, section, onChangeSection }) => {
   useEffect(() => {
+    const parentSection = document.querySelector('#WrapperSection')
     const checkSectinIsVisible = () => {
       const sectionElement = document.querySelector(`#${section.name}`)
       const positionSection = sectionElement?.getBoundingClientRect()
+      const containerRect = parentSection?.getBoundingClientRect()
 
       if (
         positionSection &&
-        sectionElement &&
-        positionSection.top >= 0 &&
-        positionSection.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight)
+        containerRect &&
+        positionSection.top >= containerRect.top &&
+        positionSection.bottom <= containerRect.bottom
       ) {
         onChangeSection?.(section)
         return
       }
     }
-
-    const parentSection = document.querySelector('#WrapperSection')
 
     if (parentSection) {
       parentSection.addEventListener('scroll', checkSectinIsVisible)
